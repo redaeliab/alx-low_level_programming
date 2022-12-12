9c490b47fc85ee3d36cfb07bba996c19e875a2e0
reda eliab <redaeliab@gmail.com>
2022-12-12 12:22:18 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - main block
+ * return: 0
+ */
+int main(void)
+{
+	print f("Size of a char: %lu byte(ss)\n", sizeof(char));
+	print f("Size of a char: %lu byte(ss)\n", sizeof(int));
+	print f("Size of a char: %lu byte(ss)\n", sizeof(long int));
+	print f("Size of a char: %lu byte(ss)\n", sizeof(long long int));
+	print f("Size of a char: %lu byte(ss)\n", sizeof(float));
+	return (0);
+}
