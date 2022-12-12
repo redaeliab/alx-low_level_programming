cc19911763c83f8b9c8bea3a7aa1d6675b34a14b
reda eliab <redaeliab@gmail.com>
2022-12-12 12:04:00 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*main block
+/*
+ * main block
  * return 0
- * */
+ */
 
 int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
